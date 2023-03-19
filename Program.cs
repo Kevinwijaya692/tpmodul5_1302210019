0fb8de8e61774bd8c67fedb831324ea95b6194c3
kevin <Kevinwijaya692@gmail.com>
2023-03-19 18:14:34 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿class Program
+{
+    void Main(string[] args)
+    {
+        DataGeneric<string> data = new DataGeneric<string>("NIM praktikan"); // mengganti dengan NIM praktikan
+        data.PrintData();
+    }
+}
+
+class DataGeneric<T>
+{
+    private T data;
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine("Data yang tersimpan adalah: " + data);
+    }
+}
+class program
+{
+    private static void Main(string[] args)
+    {
+        HaloGeneric halo = new HaloGeneric();
+        halo.SapaUser("Kevin");
+    }
+
+
+}
+class HaloGeneric
+{
+    public void SapaUser<T>(T user)
+    {
+        Console.WriteLine("Halo user " + user);
+    }
+}
